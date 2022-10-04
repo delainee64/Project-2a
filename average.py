325d42422f82e52643ff7d7ab7c80fd0d9e6a615
Delainee Lenss <lenssd@oregonstate.edu>
2022-10-03 19:41:04 -0500

    Commit Message

---
+++ average.py
@@ -0,0 +1,15 @@
+# Author: Delainee Lenss
+# Github username: delainee64
+# Date: 10/02/2022
+# Description: Asks the user for five numbers and then prints the average of the numbers.
+
+print("Please enter five numbers.")
+first_num = float(input())
+second_num = float(input())
+third_num = float(input())
+fourth_num = float(input())
+fifth_num = float(input())
+result = first_num + second_num + third_num + fourth_num + fifth_num
+result /= 5
+print("The average of those numbers is:")
+print(result)
